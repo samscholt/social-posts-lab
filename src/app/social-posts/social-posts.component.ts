bec8422f2677a99e830806973e2c33ea9558e001
sam <thesamtastic1@gmail.com>
2020-01-29 18:27:19 -0500

    Form pops up correctly

--- src/app/social-posts/social-posts.component.ts
+++ src/app/social-posts/social-posts.component.ts
@@ -8,15 +8,23 @@ import { IPost } from '../interfaces/IPost';
 })
 export class SocialPostsComponent implements OnInit {
   postList = [{title: "Real, non-hard coded sample post", thought: "Hello world!"}];
+  formVisible : boolean;
 
-  constructor() { }
+  constructor() { 
+    this.formVisible = false;
+  }
 
   onSubmit(post : IPost) {
     this.postList.unshift(post);
+    this.toggleVisibility();
   }
 
   onDelete(i : number) {
     this.postList.splice(i, 1);
+  }
+
+  toggleVisibility() {
+    this.formVisible = !this.formVisible;
   }
 
   ngOnInit() {
